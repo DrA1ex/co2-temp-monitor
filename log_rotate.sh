e40296483487d3805495f9899e4c716aa3da0d8b
Alexander <DrA1ex@users.noreply.github.com>
2023-12-07 00:14:24 +0500

    Allow to customize sensor's log file in log_rotate

--- log_rotate.sh
+++ log_rotate.sh
@@ -1,11 +1,12 @@
 BASEDIR="${BASEDIR=$(dirname "$0")}"
+FILE_KEY="${FILE_KEY="temp"}"
 LINES=3000
 
 LOG_DIR="${BASEDIR}/logs"
 
 time=$(date '+%Y-%m-%dT%H-%M-%S')
-name="${LOG_DIR}/temp_${time}.log"
+name="${LOG_DIR}/${FILE_KEY}_${time}.log"
 
 mkdir -p "${LOG_DIR}"
-cp "${BASEDIR}/temp.log" "${name}"
-tail -n ${LINES} < "${name}" > "${BASEDIR}/temp.log"
+cp "${BASEDIR}/${FILE_KEY}.log" "${name}"
+tail -n ${LINES} < "${name}" > "${BASEDIR}/${FILE_KEY}.log"
