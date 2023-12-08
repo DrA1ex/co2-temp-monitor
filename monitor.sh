feed176ec31ff1f970cf4a61277a519475e541fb
Alexander <DrA1ex@users.noreply.github.com>
2023-12-08 20:31:09 +0500

    Restart for any exit reason (monitor.sh)

--- monitor.sh
+++ monitor.sh
@@ -6,12 +6,6 @@ while true; do
     # Start the process
     $COMMAND
 
-    # Check if the process exited normally
-    if [ $? -eq 0 ]; then
-        echo "Process exited normally, not restarting."
-        break
-    else
-        echo "Process exited abnormally, restarting."
-        sleep 3  # Add a delay before restart to prevent instant respawn
-    fi
+    echo "Process exited, restarting."
+    sleep 3  # Add a delay before restart to prevent instant respawn
 done
