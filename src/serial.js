a561f9a39d345a60957f4be69a4546747be45633
Alexander <DrA1ex@users.noreply.github.com>
2023-12-08 19:28:37 +0500

    Implemented serial port receiver

---
+++ src/serial.js
@@ -0,0 +1,25 @@
+import {ReadlineParser, SerialPort} from 'serialport'
+import fs from "node:fs/promises";
+
+import {JSONPreset} from "lowdb/node";
+import process from "node:process";
+
+const SERIAL_PORT = process.env.SERIAL;
+
+const db = await JSONPreset('db.json', {});
+if (!db.data.Settings?.fileName) throw new Error("Database not configured!");
+
+const port = new SerialPort({path: SERIAL_PORT, baudRate: 9600, autoOpen: false});
+
+port.pipe(new ReadlineParser()).on("data", async (data) => {
+    const out = `${new Date().toISOString()}\t${data.toString().trim()}`;
+    console.log(out);
+
+    await fs.appendFile(db.data.Settings.fileName, out + "\n");
+})
+
+console.log(`Connecting to ${SERIAL_PORT}`);
+port.open(err => {
+    if (err) return console.log("Unable to connect: ", err);
+    console.log("Connected");
+});
